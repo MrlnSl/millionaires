8dddf4ed0f1bf731ea85b321b2fa78df6303670a
Marlena <slusarczyk.marlena@wp.pl>
2022-01-25 13:57:49 +0100

    add questions generator

--- src/main/java/pl/marlens/millionaires/services/QuizDataService.java
+++ src/main/java/pl/marlens/millionaires/services/QuizDataService.java
@@ -3,7 +3,11 @@ package pl.marlens.millionaires.services;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import pl.marlens.millionaires.dto.CategoriesDto;
+import pl.marlens.millionaires.dto.QuestionsDto;
+
+import java.net.URI;
 
 @Service
 @Log
@@ -13,5 +17,19 @@ public class QuizDataService {
         RestTemplate restTemplate = new RestTemplate();
         CategoriesDto result = restTemplate.getForObject("https://opentdb.com/api_category.php", CategoriesDto.class);
         log.info("Quiz categories: " + result.getCategories());
+    }
+
+    public void getQuizQuestions() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://opentdb.com/api.php")
+                .queryParam("amount", 2)
+                .queryParam("category", 25)
+                .queryParam("difficulty", "medium")
+                .build().toUri();
+        log.info("Quiz question retrieve URL: " + uri);
+
+        QuestionsDto result = restTemplate.getForObject(uri, QuestionsDto.class);
+        log.info("Quiz questions: " + result.getResults());
     }
 }
